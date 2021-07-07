3fd57ad799b6f0b72757828ea96ebb5f48838911
David Detweiler <daviddetweiler1@gmail.com>
2021-07-07 11:36:27 -0700

    Revert "Checkpoint spinning cube"
    This reverts commit 2f491a00bdd5d701d587820895b340e844d0ede8.

--- runtime/vertex.hlsl
+++ runtime/vertex.hlsl
@@ -1,20 +1,14 @@
 struct constant_data {
-	float4x4 projection;
-	float4x4 view;
-};
-
-struct vertex_output {
-	float3 color : COLOR;
-	float4 position : SV_POSITION;
+	matrix projection;
+	matrix view;
 };
 
 ConstantBuffer<constant_data> constants : register(b0);
 
-[RootSignature("RootConstants(num32BitConstants=32,b0), RootFlags(ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT)")] vertex_output
-main(float3 position
-	 : SV_POSITION) {
-	vertex_output output;
-	output.position = mul(constants.projection, mul(constants.view, float4(position, 1.0f)));
-	output.color = position;
-	return output;
+[RootSignature("RootConstants(num32BitConstants=32,b0), RootFlags(ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT)")] float4
+main(float4 position
+	 : SV_POSITION) :
+	SV_POSITION
+{
+	return mul(constants.view, position);
 }
